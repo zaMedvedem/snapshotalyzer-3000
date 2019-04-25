0800abe5f2b75262de4eb61fe67879c99761626b
Yevgen Reus <yevgenreus@zaMedvedem.local>
2019-04-25 14:48:33 -0500

    Don't start a snapshot if there is already one pending

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -105,7 +109,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
-            print("     Creating snapshots of {0}".format(v.id))
+            if has_pending_snapshot(v):
+                    print("     Skipping {0}, snapshot already in pogress".format(v.id))
+                    continue
+            print("     Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyzer 3000")
 
         print("Starting {0}...".format(i.id))
